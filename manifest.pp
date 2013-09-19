ddd00b4febfee23d81571c307db9cb5ab272a528
Marji Cermak <cermakm@gmail.com>
2013-09-19 10:59:17 +0200

    Adding php-apc package and deploying APC dashboard.

--- manifest.pp
+++ manifest.pp
@@ -32,15 +32,32 @@ exec { 'apt-update':
 Exec['apt-update'] -> Package <| |>
 
 
+# Include minimal apache2 installation. Munin server, nagios and APC dashboard depend on it.
+include 'apache2'
+
+
+# Install munin node and munin server:
 class { 'munin::node': }
 class { 'munin::server':
   htuser => 'munin',      # Feel free to change. Username for basic access auth.
   htpass => 'Prague2013'  # Feel free to change. Password for basic access auth.
 } 
 
+# Install nagios:
 class { 'nagios::server':
   contact_email => 'root@localhost', # Feel free to change. Email to send alerts to.
   htpass        => 'Prague2013',     # Feel free to change. Password for the nagiosadmin username.
   # note: the nagiosadmin username is the default which comes with the nagios3 package.
+}
+
+
+# Deployes APC dashboard - install php-apc package and deploy the apc.php script from it.
+package { 'php-apc': ensure => installed }
+exec { 'deploy-apc-dashboard':
+  path    => '/bin:/usr/bin',
+  command => 'gzip -dc /usr/share/doc/php-apc/apc.php.gz > /var/www/apc.php', 
+  notify  => Service['apache2'],
+  unless  => '[ -f /var/www/apc.php ]',
+  require => [ Package['php-apc'], Package['apache2'] ]
 }
 
